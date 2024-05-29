3a94e054988fc9f6746e790bd29937af41a608c8
MatheusHaliski <120330198+MatheusHaliski@users.noreply.github.com>
2024-05-29 11:37:52 -0300

    Add files via upload

--- lista-usuario/lista-usuario.component.ts
+++ lista-usuario/lista-usuario.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ProdutoService } from '../produto.service';
 
 @Component({
   selector: 'app-lista-usuario',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-usuario.component.css']
 })
 export class ListaUsuarioComponent {
+  @ViewChild('modalEditar') modalEditarRef!: ElementRef<HTMLDivElement>;
+  usuarios: any[] = []; // Array para armazenar os usuários
+  usuarioEditado: any = []; // Declaração da propriedade usuarioEditado
 
+  constructor(private produtoservice: ProdutoService) { }
+
+  ngAfterViewInit() {
+    this.getUsuarios();
+  }
+
+  openModalEditar(usuario: any) {
+    const modalEditar = this.modalEditarRef.nativeElement;
+    modalEditar.style.display = "block";
+    this.usuarioEditado = { ...usuario }; // Copia os detalhes do usuário para edição
+  }
+
+  closeModalEditar() {
+    const modalEditar = this.modalEditarRef.nativeElement;
+    modalEditar.style.display = "none";
+  }
+
+  salvarEdicao() {
+    this.produtoservice.editarUsuario(this.usuarioEditado.key, {
+      nome: this.usuarioEditado.nome,
+      dataNascimento: this.usuarioEditado.dataNascimento
+    }).then(() => {
+      console.log("Usuário editado:", this.usuarioEditado.key);
+      this.closeModalEditar();
+    }).catch(error => {
+      console.error("Erro ao editar usuário:", error);
+    });
+  }
+
+  getUsuarios() {
+    this.produtoservice.getUsuarios().subscribe(usuarios => {
+      this.usuarios = usuarios;
+    });
+  }
+
+  excluirUsuario(key: string) {
+    this.produtoservice.excluirUsuario(key).then(() => {
+      console.log("Usuário excluído:", key);
+    }).catch(error => {
+      console.error("Erro ao excluir usuário:", error);
+    });
+  }
 }
