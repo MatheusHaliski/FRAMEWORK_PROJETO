54eb4363c83366714294f724a721af6f1b108a63
MatheusHaliski <120330198+MatheusHaliski@users.noreply.github.com>
2024-05-29 11:38:50 -0300

    Add files via upload

--- lista-produto/lista-produto.component.ts
+++ lista-produto/lista-produto.component.ts
@@ -1,39 +1,86 @@
-import { Component,ViewChild,ElementRef } from '@angular/core';
-import { Form, FormControl, Validators,FormGroup, FormBuilder } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire/compat';
-import { FirestoreModule } from '@angular/fire/firestore';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ProdutoService } from '../produto.service';
-import { FormsModule } from '@angular/forms';
-import { AngularFirestore } from '@angular/fire/compat/firestore'; // Importe o AngularFirestore
+
 @Component({
   selector: 'app-lista-produto',
   templateUrl: './lista-produto.component.html',
   styleUrls: ['./lista-produto.component.css']
 })
 export class ListaProdutoComponent {
-  @ViewChild('myModal') modalRef!: ElementRef<HTMLDivElement>; // Referência para o modal
+  @ViewChild('modalEditar') modalEditarRef!: ElementRef<HTMLDivElement>;
+  @ViewChild('modalAgendar') modalAgendarRef!: ElementRef<HTMLDivElement>; // Referência para o modal de agendamento
   produtos: any[] = []; // Array para armazenar os produtos
+  produtoEditado: any = []; // Declaração da propriedade produtoEditado
+  dataVisita: string = '';
+  horaVisita: string = '';
+  idUsuario: string = '';
+  nome: string = '';
+  constructor(private produtoService: ProdutoService) { }
 
-  constructor(private firestore: AngularFirestore) {} // Injete o AngularFirestore no construtor
-
-  ngOnInit() {
-    this.getProdutos(); // Chame a função para obter os produtos ao inicializar o componente
+  ngAfterViewInit() {
+    this.getProdutos();
   }
 
-  openModal() {
-    const modal = this.modalRef.nativeElement;
-    modal.style.display = "block";
+  openModalEditar(produto:any) {
+    this.nome = produto.nome;
+    const modalEditar = this.modalEditarRef.nativeElement;
+    modalEditar.style.display = "block";
+    this.produtoEditado = { ...produto }; // Copia os detalhes do produto para edição
   }
 
-  closeModal() {
-    const modal = this.modalRef.nativeElement;
-    modal.style.display = "none";
+  closeModalEditar() {
+    const modalEditar = this.modalEditarRef.nativeElement;
+    modalEditar.style.display = "none";
   }
 
-  // Função para obter os produtos do Firestore
+  salvarEdicao() {
+    this.produtoService.editarProduto(this.produtoEditado.key, {
+      nome: this.produtoEditado.nome,
+      especie: this.produtoEditado.especie
+    }).then(() => {
+      console.log("Produto editado:", this.produtoEditado.key);
+      this.closeModalEditar();
+    }).catch(error => {
+      console.error("Erro ao editar produto:", error);
+    });
+  }
+
+  openModalAgendar(produto: any) {
+    this.produtoEditado = produto;
+    const modalAgendar = this.modalAgendarRef.nativeElement;
+    modalAgendar.style.display = "block";
+    // Lógica para exibir o modal de agendamento
+  }
+
+  closeModalAgendar() {
+    const modalAgendar = this.modalAgendarRef.nativeElement;
+    modalAgendar.style.display = "none";
+    // Lógica para fechar o modal de agendamento
+  }
+
+  confirmarAgendamento() {
+    this.nome = this.produtoEditado.nome;
+    console.log(this.produtoEditado);
+    this.produtoService.agendarVisita(this.nome,this.produtoEditado.key, this.dataVisita, this.horaVisita,this.idUsuario).then(() => {
+      console.log("Visita agendada para o produto:", this.produtoEditado.key);
+      this.closeModalAgendar();
+    }).catch(error => {
+      console.error("Erro ao agendar visita:", error);
+    });
+  }
+  
+
   getProdutos() {
-    this.firestore.collection('produtos').valueChanges().subscribe((data: any[]) => {
-      this.produtos = data; // Atribua os dados ao array de produtos
+    this.produtoService.getProdutos().subscribe(produtos => {
+      this.produtos = produtos;
+    });
+  }
+
+  excluirProduto(key: string) {
+    this.produtoService.excluirProduto(key).then(() => {
+      console.log("Produto excluído:", key);
+    }).catch(error => {
+      console.error("Erro ao excluir produto:", error);
     });
   }
 }
